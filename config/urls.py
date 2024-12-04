1872d195738a77471a2e3eda0da0f5d5f60d06f4
Ehsan Doroudian <ehsan.doroudian@gmail.com>
2024-12-04 14:09:12 +0330

    add order checkout page

--- config/urls.py
+++ config/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path("accounts/", include("allauth.urls")),
     path("products/", include("products.urls")),
     path("cart/", include("cart.urls")),
+    path("orders/", include("orders.urls")),
     path("", include("products.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,24 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Order, OrderItem
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    fields = ['order', 'product', 'quantity', 'price']
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['user', 'first_name', 'last_name', 'datetime_created', 'is_paid']
+    inlines = [OrderItemInline]
+
+
+admin.site.register(Order, OrderAdmin)
+
+
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ['order', 'product', 'quantity', 'price']
+
+
+admin.site.register(OrderItem, OrderItemAdmin)
